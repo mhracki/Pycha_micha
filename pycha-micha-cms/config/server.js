52e51718373650f0351430e1ad5eadc85064bb79
Martyna Kruczek <martyna.kruczek96@gmail.com>
2021-01-13 21:18:20 +0100

    'remove

--- pycha-micha-cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b7ff691b1b0d7c856b539f49d8fac1fd'),
-    },
-  },
-});
--- pycha-micha-cms/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '32da5fd5-41ad-4c19-9ee6-82dff084d23e'
-};
