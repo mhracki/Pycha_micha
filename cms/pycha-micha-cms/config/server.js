82f5588776b92d5056c25a954a6f48cac36645a3
Martyna Kruczek <martyna.kruczek96@gmail.com>
2021-01-13 21:11:40 +0100

    'remove

--- cms/pycha-micha-cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6fafa8b8ae1f36aaf037cbe437cf1b18'),
-    },
-  },
-});
